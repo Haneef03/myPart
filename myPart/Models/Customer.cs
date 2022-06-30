48faa3543ea07239e4d0e4e5db5c2ea2ca41069a
Haneef03 <s10223481@connect.np.edu.sg>
2022-06-30 19:07:44 +0800

    Add project files.

---
+++ myPart/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myPart.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int DOB { get; set; }
+        public int PhoneNumber { get; set;}
+        public string EmailAddress { get; set; }
+    }
+}
